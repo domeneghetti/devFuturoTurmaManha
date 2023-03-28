a6b0dd36cc436e0a819aa46076cd77695a6c6877
Fernando Domeneghetti <fernando.domeneghetti@totvs.com.br>
2023-03-28 08:39:55 -0300

    projeto turma da manha

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+const urlEncondedParser = bodyParser.urlencoded({ extended: false })
+
+function somar(numeroUm, numeroDois) {
+    return parseInt(numeroUm) + parseInt(numeroDois);
+}
+
+function main() {
+    console.log('Hello World')
+
+    var numUm = 10;
+    var numDois = 22;
+
+
+    const resultado = somar(numUm, numDois);
+    console.log('Resultado é', resultado)
+}
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+
+app.post('/calc', urlEncondedParser, function(req, res){
+    console.log('entrou no post', req.body)
+    const resultado = somar(req.body.numeroUm, req.body.numeroDois);
+
+    res.send({ resultado: resultado })
+})
+  
+app.listen(3000)
